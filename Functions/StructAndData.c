c81c3736bd4e6747d9730b38cada5cc601fd9c8b
estevaoanjo <estevaosilvino25@gmail.com>
2026-04-10 00:06:53 -0300

    Testing of the save the operations

--- Functions/StructAndData.c
+++ Functions/StructAndData.c
@@ -8,10 +8,10 @@
 // Verificar espaços vazios e criar condições
 
 typedef struct{
-  int nm1[10];
-  char oprd[10];
-  int nm2[10];
-  int res[10];
+  int nm1;
+  char oprd;
+  int nm2;
+  int res;
 } calculadora ;
 calculadora mostrar[20];
 
@@ -19,9 +19,10 @@ void clearscreen(){
   system("cls");
  }
 
-int mostrarCalculos(calculadora mostrar[20], int num1,char opr, int num2, int resultado);
+int mostrarCalculos(calculadora mostrar[20], int num1,char opr, int num2, int resultado, int tam);
 int fazerCalculos();
 int subMain(char operador, int res, int nm1, int nm2);
+int moreOperations(int opcaoMain);
 
 
 
@@ -41,25 +42,25 @@ int main(){
 }
 
 
-int mostrarCalculos(calculadora mostrar[20], int num1,char opr, int num2, int resultado){
-          
-    int tam = 1;
-    int i;
+int mostrarCalculos(calculadora mostrar[20], int num1,char opr, int num2, int resultado, int tam){
+   
+   int i;
     char opc;
 
-    printf("\nHistórico de calculos: ");
+    printf("\nHistórico de calculos (S | N): ");
     scanf(" %c", &opc);
     if((opc == 'S')||(opc == 's')){
       
         if((opr == '+')||(opr == '-')||(opr == '*')||(opr == '/')){ 
             for( i = 0 ; i < tam ; i++){
-              mostrar[i].nm1[i]= num1;
-              mostrar[i].oprd[i] = opr;
-              mostrar[i].nm2[i] = num2;
-              mostrar[i].res[i] = resultado;
-              printf("| Calculo: %d %c %d: %d\n", mostrar[i].nm1[i],mostrar[i].oprd[i],mostrar[i].nm2[i], mostrar[i].res[i]);
+              mostrar[i].nm1= num1;
+              mostrar[i].oprd = opr;
+              mostrar[i].nm2 = num2;
+              mostrar[i].res = resultado;
+               printf("\n| Calculo: %d %c %d: %d\n", mostrar[i].nm1, mostrar[i].oprd, mostrar[i].nm2, mostrar[i].res);
             }
-        } else
+        }
+         else
         printf("Operador não encontrado!");
 
     } else {
@@ -77,7 +78,8 @@ int fazerCalculos(){
     int nm1, nm2;
     //int pos = 10;
     char operador;
-    int res, opcaoMain;
+    int res, opcaoMain = 0;
+    int tamanho;
 
      printf("\n\nDigite um numero: ");
      scanf("%d", &nm1);
@@ -87,18 +89,13 @@ int fazerCalculos(){
      scanf("%d", &nm2);
       
      subMain(operador, res, nm1, nm2);
-
-     printf("\nFazer mais calculos? 1-Sim 2-Não\n");
+     tamanho++;
+     mostrarCalculos(mostrar, nm1, operador, nm2, res, tamanho);
     
-     scanf(" %i", &opcaoMain);
 
-     if (opcaoMain == 1){
-       fazerCalculos();
-       mostrarCalculos(mostrar,nm1,operador, nm2, res);
-      } else if (opcaoMain == 2){ 
-        printf("\nOK! Muito Obrigado por utilizar nossos serviços!. "); 
-        return mostrarCalculos(mostrar,nm1,operador, nm2, res);
-      } else { printf("\nEscolha uma opção válida!\n"); }
+     printf("\n\n Fazer mais calculos ( 1 - Sim  2 - Nao ): ");
+     scanf(" %i", &opcaoMain);
+     moreOperations(opcaoMain);
 
       
        return 0;
@@ -130,5 +127,22 @@ int subMain(char operador, int res, int nm1, int nm2) {
 
 
   return 1;
+
 }
 
+int moreOperations(int opcaoMain){
+
+   if (opcaoMain == 1){
+
+       fazerCalculos();
+
+      } else if (opcaoMain == 2){ 
+
+        printf("\nOK! Muito Obrigado por utilizar nossos serviços!. "); 
+        return 0;
+
+      } else {
+         printf("\nEscolha uma opção válida!\n"); 
+        }
+
+}
