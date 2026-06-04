40ce13cbae5edddf6f65ff09a2476fe4ac1ac603
estevaoanjo <estevaosilvino25@gmail.com>
2026-06-03 21:21:21 -0300

    Removal of a file in the 'Output' folder that no longer existed, and also the completion of the 'Sorting Algorithms' function

--- Functions/AlgoritmosOrdenacao.c
+++ Functions/AlgoritmosOrdenacao.c
@@ -5,15 +5,23 @@ int buscarMaiorNumero(int v[], int tam);
 void SelectSort(int array[], int tam);
 void BubbleSort(int array[], int tamanho);
 void QuickSort(int array[], int tam);
+void merge(int lista[], int inicio, int meio, int fim);
+void mergeSort(int lista[], int inicio, int fim);
 void impressao(int array[], int tam);
 
 int main(){
-    int numberDesordanation[]={30,23,322,33,743,1,0,4905,86};
+    int numberDesordanation[]={30, 23, 322, 33, 743, 1, 0, 4905, 86, 342, 15, 987, 123,
+                               56, 781, 29, 444, 3, 652, 89, 392, 999, 31, 39, 
+                               199, 874, 21, 555, 430, 76, 928, 310, 87, 502,
+                               11, 723, 48, 600, 275, 934, 167, 390, 8, 811,
+                               221, 470, 59, 150, 703, 42, 980, 266, 514, 18,
+                               647, 91, 333, 720, 5, 880, 124, 461, 299, 771
+  };
     int tam = sizeof(numberDesordanation)/sizeof(numberDesordanation[0]);
     int opc, op;
     printf("\nLista desordenada: ");
     for(int i = 0; i < tam ; i++){      
-    printf(" %d", numberDesordanation[i]);
+    printf(" %d  ", numberDesordanation[i]);
     }
     printf("\n");
 
@@ -26,16 +34,20 @@ int main(){
        printf(" %d", buscarMaiorNumero(numberDesordanation,tam));
        break;
     case 2:
-      printf("\n 1- Selection Sort || 2- Bubble Sort  || 3- Quick Sort\n");
+      printf("\n 1- Selection Sort || 2- Bubble Sort  || 3- Quick Sort  || 4- Merge Sort\n");
       scanf("%i", &op);
       if(op == 1){
-         SelectSort(numberDesordanation, tam);
+        SelectSort(numberDesordanation, tam);
       } else if(op == 2){
-        BubbleSort(numberDesordanation, tam);
+         BubbleSort(numberDesordanation, tam);
       } else if(op == 3){
          QuickSort(numberDesordanation, tam);
          impressao(numberDesordanation, tam);
-      } else 
+      } else if(op == 4){
+         mergeSort(numberDesordanation, 0, tam-1);
+         impressao(numberDesordanation, tam);
+      }
+      else 
         printf("Opção Inválida!");
       break;
 
@@ -64,7 +76,7 @@ void SelectSort(int array[], int tam){
 
        int indElem = i;
       for(j = i + 1 ; j < tam ; j++){
-          if( array[j] > array[indElem]){
+          if( array[j] < array[indElem]){
               indElem = j;
           }
       }
@@ -128,6 +140,44 @@ void QuickSort(int array[], int tam){
             }  
           
       }
+}
+
+void merge(int lista[], int inicio, int meio, int fim){
+ int i, j, k, n1 = meio - inicio + 1;
+ int n2 = fim - meio;
+
+ int *esquerda = malloc(n1* sizeof(int));
+ int *direita = malloc(n2* sizeof(int));
+
+ for( i=0; i < n1 ; i++)
+    esquerda[i] = lista[inicio+i];
+ for( j=0 ; j< n2 ; j++)
+    direita[j] = lista[meio+1+j];
+  
+  i =0, j =0, k = inicio;
+   while(i < n1 && j <n2){
+    if(esquerda[i] <= direita[j])
+      lista[k++] =esquerda[i++];
+    else
+      lista[k++]=direita[j++];
+   }
+  
+   while(i<n1)
+    lista[k++]=esquerda[i++];
+   while(j<n2)
+    lista[k++]= direita[j++];
+
+ free(esquerda);
+ free(direita);
+}
+void mergeSort(int lista[], int inicio, int fim){
+    if(inicio < fim){
+     int meio =(inicio + fim)/2 ;
+
+     mergeSort(lista, inicio, meio);
+     mergeSort(lista, meio+1, fim);
+     merge(lista, inicio, meio, fim);
+    }
 }
 
 void impressao(int array[], int tam){
--- Functions/TesteAlgoritmos.c
+++ Functions/TesteAlgoritmos.c
@@ -3,71 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-// Algoritmo de ordenação Merge Sort
-
-void merge(int lista[], int inicio, int meio, int fim){
- int i, j, k, n1 = meio - inicio + 1;
- int n2 = fim - meio;
-
- int *esquerda = malloc(n1* sizeof(int));
- int *direita = malloc(n2* sizeof(int));
-
- for( i=0; i < n1 ; i++)
-    esquerda[i] = lista[inicio+i];
- for( j=0 ; j< n2 ; j++)
-    direita[j] = lista[meio+1+j];
-  
-  i =0, j =0, k = inicio;
-   while(i < n1 && j <n2){
-    if(esquerda[i] <= direita[j])
-      lista[k++] =esquerda[i++];
-    else
-      lista[k++]=direita[j++];
-   }
-  
-   while(i<n1)
-    lista[k++]=esquerda[i++];
-   while(j<n2)
-    lista[k++]= direita[j++];
-
- free(esquerda);
- free(direita);
-}
-
-void mergeSort(int lista[], int inicio, int fim){
-    if(inicio < fim){
-     int meio =(inicio + fim)/2 ;
-
-     mergeSort(lista, inicio, meio);
-     mergeSort(lista, meio+1, fim);
-     merge(lista, inicio, meio, fim);
-    }
-}
-
-
-void imprimirMS(int lista[], int tam){
-  int k;
-  for(k = 0; k < tam; k++)
-    printf("%d ", lista[k]);
-  printf("\n");
-}
-
-
-void preencherLista(int lista[], int tam){
-  int i;
-  int max = 100;
-  int min = 1;
-  for(i = 0; i< tam; i++)
-   lista[i] = rand() % (max - min + 1) + min;
-}
+// Algoritmo de grafos
 
 int main(){
-    srand(time(NULL));
-    int tam = 50;
-    int lista[tam];
-    
-    preencherLista(lista, tam);
-    mergeSort(lista, 0, tam-1);
-    imprimirMS(lista, tam);
-
+  return 0;
 }
